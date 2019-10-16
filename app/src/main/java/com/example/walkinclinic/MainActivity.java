fa4b09ff2c47cd4a958e3fa286c2291a728c4c7e
asklo016 <asklo016@uottawa.ca>
2019-10-16 16:04:30 -0400

    UI Final Product

--- app/src/main/java/com/example/walkinclinic/MainActivity.java
+++ app/src/main/java/com/example/walkinclinic/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.walkinclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToRegister(View v){
+        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    public void goToLogin(View v){
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivityForResult(intent, 0);
     }
 }
