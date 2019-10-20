1945b05b3e955e556978f21d992c25eb57fdc508
Matthew Tran <mtran014@uottawa.ca>
2019-10-20 00:56:54 -0700

    Implemented addUser method allowing you to add a User to Firebase with a specific User(object) in the parameter

--- app/src/main/java/DBFunc.java
+++ app/src/main/java/DBFunc.java
@@ -1,7 +1,7 @@
-public interface DBFunc<U> {
-    public void addUser(U input); //Database functionality for adding a user to the database
-    public U getUser(String input); //Database functionality for obtaining a user from the database
+public interface DBFunc {
+    boolean addUser(User input); //Database functionality for adding a user to the database
+    User getUser(String input); //Database functionality for obtaining a user from the database
     //Input will be username (only input for now)
-    public void deleteUser(String input); //Database functionality for deleting a user from the database
-    public void editUser(U input); //Database functionality for editing a user's credentials inside the database
+    void deleteUser(String input); //Database functionality for deleting a user from the database
+    void editUser(User input); //Database functionality for editing a user's credentials inside the database
 }
--- app/src/main/java/Database.java
+++ app/src/main/java/Database.java
@@ -1,16 +1,5 @@
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.net.Uri;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.content.Intent;
-
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 
@@ -22,28 +11,42 @@ import com.google.firebase.auth.FirebaseUser;
 
 import static android.R.id.message;
 
-public class Database<U> implements DBFunc<U> {
+public class Database implements DBFunc {
+    //log
+    private static final String TAG = "Database";
 
-    public void addUser(U input) { //Database functionality for adding a user to the database
-        //TO_DO Matthew
+    private FirebaseAuth mAuth;
+
+
+    public boolean addUser(User input) { //Database functionality for adding a user to the database
+        String email = input.getUsername();
+        String password = input.getPassword();
+        if (!email.equals("") && !password.equals("")){
+            //call to mAuth firebase method using text inputs
+            mAuth.createUserWithEmailAndPassword(email, password);
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public U getUser(String input){ //Database functionality for obtaining a user from the database
+    public User getUser(String input){ //Database functionality for obtaining a user from the database
         //the input is a string which will be the username only
         return null;
     }
     //TO_DO Matthew
     public void deleteUser(String input){ //Database functionality for deleting a user from the database
-        //TO_DO Matthew
+        //TO_DO_LATER Matthew
     }
 
-    public void editUser(U input){ //Database functionality for editing a user's credentials inside the database
+    public void editUser(User input){ //Database functionality for editing a user's credentials inside the database
         //TO_DO Matthew
     }
 
     public boolean existsUser(String input){ //Database functionality for checking whether a certain user exists in the database or not
         //TO_DO Matthew
-        return null;
+
+        return true;
     }
 
 }
